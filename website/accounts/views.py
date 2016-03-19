52b9ec52f6207f203747ff13c7899d7fc9ba2588
jordan <jordan.chou1@gmail.com>
2016-03-19 15:25:01 +0800

    Added views for registration

---
+++ website/accounts/views.py
@@ -0,0 +1,31 @@
+from django.contrib.auth.forms import PasswordResetForm
+from django.shortcuts import redirect
+from django.views.generic import CreateView
+
+from .forms import RegistrationForm
+from .models import CustomUser
+
+class RegistrationView(CreateView):
+    form_class = RegistrationForm
+    model = CustomUser
+
+    def form_valid(self, form):
+        obj = form.save(commit=False)
+        obj.set_password(CustomUser.objects.make_random_password())
+        obj.save()
+
+        reset_form = PasswordResetForm(self.reuqest.POST)
+        reset_form.is_valid()
+        
+        # Copied from django/contrib/auth/views.py : password_reset
+        opts = {
+            'use_https': self.request.is_secure(),
+            'email_template_name': 'registration/verification.html',
+            'subject_template_name' : 'registration/verification_subject.txt',
+            'request': self.request,
+        }
+        # This form sends the email on save()
+        reset_form.save(**opts)
+
+        return redirect('accounts:register-done')
+
