68f8abdce9652ae5bc614674ccdd9a99700044f8
jordan <jordan.chou1@gmail.com>
2016-03-19 20:07:26 +0800

    Added option to change password

--- website/accounts/urls.py
+++ website/accounts/urls.py
@@ -2,11 +2,30 @@ from django.conf.urls import url
 
 from .views import RegistrationView
 from django.contrib.auth import views
-from django.contrib.auth.views import login, logout
+from django.contrib.auth.views import login, logout, password_change, password_change_done
 
 urlpatterns = [
                 url(r'^login/$', 'django.contrib.auth.views.login', name='login',
                     kwargs={'template_name': 'accounts/login.html'}
+                   ),
+
+                url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout',
+                    kwargs={'next_page': '/'}
+                   ),
+
+                url(r'^password_change$',
+                    'django.contrib.auth.views.password_change',
+                    name='password_change',
+                    kwargs={
+                            'template_name':'accounts/password_change_form.html',
+                            'post_change_redirect':'accounts:password_change_done',
+                           }
+                   ),
+
+                url(r'^password_change_done$',
+                    'django.contrib.auth.views.password_change_done',
+                    name='password_change_done',
+                    kwargs={'template_name':'accounts/password_change_done.html'}
                    ),
 
                 url(r'^register/$', RegistrationView.as_view(), name='register'),
