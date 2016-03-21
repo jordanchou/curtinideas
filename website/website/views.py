c7f34c359748524b711d1e3800531c9cf6c3d702
jordan <jordan.chou1@gmail.com>
2016-03-21 17:12:02 +0800

    Added HTML links/pages for contact_us, about_us and faq

--- website/website/views.py
+++ website/website/views.py
@@ -7,3 +7,21 @@ def index(request):
     template = loader.get_template('index.html') 
 
     return HttpResponse(template.render())
+
+def about_us(request):
+    # Load template
+    template = loader.get_template("about_us.html")
+
+    return HttpResponse(template.render())
+
+def contact_us(request):
+    # Load template
+    template = loader.get_template("contact_us.html")
+
+    return HttpResponse(template.render())
+
+def faq(request):
+    # Load template
+    template = loader.get_template("faq.html")
+
+    return HttpResponse(template.render())
