9da9b8a5f43fa52963edac9f66b9a9a0b5650f1c
brendan <b.lally31@gmail.com>
2016-03-28 21:20:48 +0800

    added a submission form for adding submissions

--- website/submission/urls.py
+++ website/submission/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
                 url(r'^$', views.submission_list, name = 'submission_list'),
+                url(r'^submit/$', views.submission_new, name='submission_new'),
 
               ]
--- website/submission/views.py
+++ website/submission/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Submission
+from .forms import SubmissionForm
 from django.utils import timezone
 
 # Create your views here.
@@ -7,3 +8,16 @@ def submission_list(request):
     submissions = Submission.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 
     return render(request, 'submission/submission_list.html', {'submissions': submissions})
+
+def submission_new(request):
+    if request.method == "POST":
+        form = SubmissionForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            #return redirect('view_submissions')
+    else:
+        form = SubmissionForm()
+    return render(request, 'submission/submission_edit.html', {'form': form})
