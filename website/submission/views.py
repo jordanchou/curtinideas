59dfd5aa924334c1870685be8b651cc1e5106a3f
jordan <jordan.chou1@gmail.com>
2016-03-28 11:16:25 +0800

    Added view for viewing submissions

--- website/submission/views.py
+++ website/submission/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Submission
+from django.utils import timezone
 
 # Create your views here.
+def submission_list(request):
+    submissions = Submission.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+
+    return render(request, 'submission/submission_list.html', {'submissions': submissions})
