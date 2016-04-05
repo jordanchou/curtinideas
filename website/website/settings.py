fd859c244df604935fc99fa66efbf354c326c858
Connor Beardsmore <connor.beardsmore@gmail.com>
2016-04-05 00:11:53 +0000

    pythonanywhere Static files setup + admin static files

--- website/website/settings.py
+++ website/website/settings.py
@@ -133,10 +133,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_PATH = os.path.join(PROJECT_PATH,'static')
+# STATIC_PATH = os.path.join(PROJECT_PATH,'static')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/' # You may find this is already defined as such.
 
 STATICFILES_DIRS = (
-    STATIC_PATH,
+
 )
