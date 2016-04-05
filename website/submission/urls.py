3b7872d765f71fa07cb6795f92413d32d2735d92
brendan <b.lally31@gmail.com>
2016-04-05 14:37:50 +0800

    Added voting for submissions

--- website/submission/urls.py
+++ website/submission/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
                 url(r'^submission(?P<pk>\d+)/$', views.submission_detail, name='submission_detail'),
                 url(r'^$', views.submission_list, name = 'submission_list'),
                 url(r'^submit/$', views.submission_create, name='submission_create'),
+                url(r'^upvotes(?P<pk>\d+)/$', views.update_upvotes, name='submission_upvotes'),
+                url(r'^downvotes(?P<pk>\d+)/$', views.update_downvotes, name='submission_downvotes'),
               ]
