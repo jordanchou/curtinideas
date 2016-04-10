555f56a7954c6f508bd0b0a1be9e455bacdab68a
jordan <jordan.chou1@gmail.com>
2016-04-10 20:15:35 +0800

    Fixed deleting, used to delete ALL submissions, not just one

--- website/submission/models.py
+++ website/submission/models.py
@@ -17,22 +17,22 @@ class Submission(models.Model):
     downvotes = models.PositiveSmallIntegerField(default=0)
     num_views = models.PositiveSmallIntegerField(default=0)
     category = models.CharField(max_length=50)
-    submission_id = models.PositiveSmallIntegerField(null=True)
+    #submission_id = models.PositiveSmallIntegerField(null=True)
 
     def post(self):
         self.published_date = timezone.now()
         self.num_views = 0
         self.upvotes = 0
         self.downvotes = 0
-        self.submission_id = id + 1
+        #self.submission_id = id + 1
         id = id + 1
         self.save()
 
     def __str__(self):
         return self.title
 
-    def get_id(self):
-        return self.submission_id
+    #def get_id(self):
+    #    return self.submission_id
 
     def get_score(self):
         if ( (self.upvotes - self.downvotes) < 0 ):
@@ -47,4 +47,4 @@ class Submission(models.Model):
         return self.downvotes
 
     def delete_submission(self):
-        Submission.objects.filter(submission_id=self.submission_id).delete()
+        Submission.objects.filter(submission_id=self.pk).delete()
--- website/submission/urls.py
+++ website/submission/urls.py
@@ -10,8 +10,8 @@ urlpatterns = [
                 url(r'^sort_by_downvotes/$', views.submission_list_downvotes, name = 'submission_list_downvotes'),
                 url(r'^sort_by_views/$', views.submission_list_num_views, name = 'submission_list_num_views'),
                 url(r'^submit/$', views.submission_create, name='submission_create'),
-                url(r'^/(?P<slug>.*)/$', views.submission_list_self, name='account_submission'),
+                url(r'^submission/(?P<slug>.*)/$', views.submission_list_self, name='account_submission'),
                 url(r'^upvotes(?P<pk>\d+)/$', views.update_upvotes, name='submission_upvotes'),
                 url(r'^downvotes(?P<pk>\d+)/$', views.update_downvotes, name='submission_downvotes'),
-                url(r'^delete/(?P<pk>\d+)/$', views.submission_delete, name='submission_delete'),
+                url(r'^delete/(?P<pk>.*)/$', views.submission_delete, name='submission_delete'),
               ]
