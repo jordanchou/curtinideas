330a3e156644cb441344be964e90be9281ee0f30
Connor <connor.beardsmore@gmail.com>
2016-04-12 20:03:37 +0800

    git being git

--- website/accounts/views.py
+++ website/accounts/views.py
@@ -17,19 +17,6 @@ class RegistrationView(CreateView):
         obj.set_password(CustomUser.objects.make_random_password())
         obj.save()
 
-        #reset_form = PasswordResetForm(self.request.POST)
-        #reset_form.is_valid()
-        
-        # Copied from django/contrib/auth/views.py : password_reset
-        #opts = {
-        #   'use_https': self.request.is_secure(),
-        #    'email_template_name': 'accounts/verification.html',
-        #    'subject_template_name' : 'accounts/verification_subject.html',
-        #    'request': self.request,
-        #        }
-        # This form sends the email on save()
-        #reset_form.save(**opts)
-
         template = loader.get_template('accounts/registration_done.html')
 
         return HttpResponse(template.render())
