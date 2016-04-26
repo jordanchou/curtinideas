06f518c3553ad71f46bc18551239984bdec1c875
Connor Beardsmore <connor.beardsmore@gmail.com>
2016-04-26 19:57:47 +0800

    FIXED comment/improvement user authetentication bug

--- website/submission/views.py
+++ website/submission/views.py
@@ -87,11 +87,11 @@ def update_sub_upvotes(request, slug, pk):
     submission = get_object_or_404(Submission, pk=pk)
     voter = get_object_or_404(CustomUser, slug=slug)
 
-    new_vote = SubVoting();
-    new_vote.create_sub_up_vote(submission, voter)
-
-    upvote = submission.get_upvotes()
-    submission_vote = Submission.objects.filter(pk=submission.pk).update(upvotes=upvote+1)
+    if (SubVoting.objects.filter(voter=voter, submission=submission).exists() == False):
+        new_vote = SubVoting();
+        new_vote.create_sub_up_vote(submission, voter)
+        upvote = submission.get_upvotes()
+        submission_vote = Submission.objects.filter(pk=submission.pk).update(upvotes=upvote+1)
 
     return submission_list(request)
 
@@ -101,11 +101,11 @@ def update_sub_downvotes(request, slug, pk):
     submission = get_object_or_404(Submission, pk=pk)
     voter = get_object_or_404(CustomUser, slug=slug)
 
-    new_vote = SubVoting();
-    new_vote.create_sub_down_vote(submission, voter)
-
-    downvote = submission.get_downvotes()
-    submission_vote = Submission.objects.filter(pk=submission.pk).update(downvotes=downvote+1)
+    if (SubVoting.objects.filter(voter=voter, submission=submission).exists() == False):
+        new_vote = SubVoting();
+        new_vote.create_sub_down_vote(submission, voter)
+        downvote = submission.get_downvotes()
+        submission_vote = Submission.objects.filter(pk=submission.pk).update(downvotes=downvote+1)
 
     return submission_list(request)
 
@@ -115,11 +115,11 @@ def update_com_upvotes(request, slug, pk):
     comment = get_object_or_404(Comment, pk=pk)
     voter = get_object_or_404(CustomUser, slug=slug)
 
-    new_vote = ComVoting();
-    new_vote.create_com_up_vote(comment, voter)
-
-    upvote = comment.get_upvotes()
-    comment_vote = Comment.objects.filter(pk=comment.pk).update(upvotes=upvote+1)
+    if (ComVoting.objects.filter(voter=voter, comment=comment).exists() == False):
+        new_vote = ComVoting();
+        new_vote.create_com_up_vote(comment, voter)
+        upvote = comment.get_upvotes()
+        comment_vote = Comment.objects.filter(pk=comment.pk).update(upvotes=upvote+1)
 
     submission = comment.submission
     return render(request, 'submission/submission_detail.html', {'submission': submission})
@@ -130,11 +130,11 @@ def update_com_downvotes(request, slug, pk):
     comment = get_object_or_404(Comment, pk=pk)
     voter = get_object_or_404(CustomUser, slug=slug)
 
-    new_vote = ComVoting();
-    new_vote.create_com_up_vote(comment, voter)
-
-    downvote = comment.get_downvotes()
-    comment_vote = Comment.objects.filter(pk=comment.pk).update(downvotes=downvote+1)
+    if (ComVoting.objects.filter(voter=voter, comment=comment).exists() == False):
+        new_vote = ComVoting();
+        new_vote.create_com_up_vote(comment, voter)
+        downvote = comment.get_downvotes()
+        comment_vote = Comment.objects.filter(pk=comment.pk).update(downvotes=downvote+1)
 
     submission = comment.submission
     return render(request, 'submission/submission_detail.html', {'submission': submission})
@@ -244,23 +244,5 @@ def search(request):
                           { 'submissions': submissions },
                           context_instance=RequestContext(request))
 
-
-#-----------------------------------------------------------------------------
-
-def update_comment_upvotes(request, pk):
-    comment = get_object_or_404(Comment, pk=pk)
-    upvote = comment.upvotes
-    comment_vote = Comment.objects.filter(pk=comment.pk).update(upvotes=upvote+1)
-    submission = comment.submission
-    return render(request, 'submission/submission_detail.html', {'submission': submission})
-
-#-----------------------------------------------------------------------------
-
-def update_comment_downvotes(request, pk):
-    comment = get_object_or_404(Comment, pk=pk)
-    downvote = comment.downvotes
-    comment_vote = Comment.objects.filter(pk=comment.pk).update(downvotes=downvote+1)
-    submission = comment.submission
-    return render(request, 'submission/submission_detail.html', {'submission': submission})
 
 #-----------------------------------------------------------------------------
